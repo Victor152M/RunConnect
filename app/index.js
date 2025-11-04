5dfd0ff4187dda9603f42fc4fb390f55a6f73954
ivankotenko2024 <iak020805@gmail.com>
2025-11-04 16:51:52 +0100

    commit

--- app/index.js
+++ app/index.js
@@ -1,17 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text style ={styles.title}> Hi there! </Text>
+      <Text> Hi there! </Text>
     </View>
   );
 }
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  title: { fontSize: 26, fontWeight: '700', textAlign: 'center', color: '#530c0cff'}
-})
-
