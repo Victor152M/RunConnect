ad0bd0576056ac78f0c2cb7af905fbed53463968
Victor152M <stanciuvictorcristian@yahoo.com>
2025-11-04 18:34:10 +0200

    preparation

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text> Hi there! Modification number 2 </Text>
+      <Text style={styles.title}> Hi there! Modification number 2 </Text>
     </View>
   );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  title: { fontSize: 24, textAlign: 'center', color: '#08e6ffa5'}
+})
