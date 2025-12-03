815c4e93d90444cb6db91449da89350b8a750712
ivankotenko2024 <iak020805@gmail.com>
2025-12-03 14:55:50 +0100

    changes

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
+import { StepsProvider } from "context/StepsContext"; // ← исправить путь
 import { Stack } from "expo-router";
 import { StyleSheet, View } from "react-native";
-import { StepsProvider } from "../context/StepsContext"; // ← исправить путь
 // если файл называется SteoContext.tsx, ПЕРЕИМЕНУЙ в StepsContext.tsx
 
 export default function RootLayout() {
